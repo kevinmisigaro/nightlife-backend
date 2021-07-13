2cca1f84cb71ba4caafeac91ac77a237cccfd9e7
luckykun88 <kmisigaro@outlook.com>
2021-07-13 22:38:05 +0300

    changes

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const Pool = require('pg')
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
 const app = express()
 app.use(cors())
-
-const Client = require('pg')
-
-const client = new Client.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
-
-client.connect()
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
 
 const clubRouter = require('./routes/club.routes')
 
@@ -30,6 +19,18 @@ app.use('/clubs', clubRouter)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening at http://localhost:3000`)
