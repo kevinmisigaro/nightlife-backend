70e68cb9941f877bf3fac435cb8faadc36231f67
luckykun88 <kmisigaro@outlook.com>
2021-07-13 22:51:54 +0300

    changes

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const Pool = require('pg')
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cors())
 
@@ -19,19 +11,7 @@ app.use('/clubs', clubRouter)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Example app listening at http://localhost:3000`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at ${PORT}`)
 })
