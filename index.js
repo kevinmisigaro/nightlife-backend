fba2f83c05a9beeb97f800507d9fd46e15e9501a
luckykun88 <kmisigaro@outlook.com>
2021-07-13 18:48:16 +0300

    Fixing port issue

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.port.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
